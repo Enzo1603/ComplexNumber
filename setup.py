d6d3d8f4df4bbeae613648b48243e68426688367
Enzo1603 <enmoba@sunrise.ch>
2022-04-14 15:16:31 +0200

    Fixes

--- setup.py
+++ setup.py
@@ -12,10 +12,11 @@ Operating System :: Unix
 Operating System :: MacOS
 """
 
-DISTNAME = 'complexnumber'
+DISTNAME = 'complex_number'
 AUTHOR = 'Enzo Baraldi'
 # AUTHOR_EMAIL = ""
 DESCRIPTION = 'This is a simple complex number python package.'
+KEYWORDS = ["python", "python3", "python3.10", "complex number", "complex numbers"]
 LICENSE = 'MIT'
 README = 'This is a simple complex number python package.'
 
@@ -27,7 +28,7 @@ PYTHON_MIN_VERSION = '3.10'
 # PYTHON_REQUIRES = f'>={PYTHON_MIN_VERSION}, <= {PYTHON_MAX_VERSION}'
 PYTHON_REQUIRES = f">={PYTHON_MIN_VERSION}"
 
-# INSTALL_REQUIRES = []
+INSTALL_REQUIRES = []
 
 PACKAGES = [
     'complex_number',
@@ -40,10 +41,11 @@ metadata = dict(
     long_description=README,
     packages=PACKAGES,
     python_requires=PYTHON_REQUIRES,
-    # install_requires=INSTALL_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
     author=AUTHOR,
     # author_email=AUTHOR_EMAIL,
     description=DESCRIPTION,
+    keywords=KEYWORDS,
     classifiers=[CLASSIFIERS],
     license=LICENSE
 )

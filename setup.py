ae599c2bc15196325c8668a3a8c33cf4fbb666e0
Enzo1603 <enmoba@sunrise.ch>
2022-04-15 21:47:37 +0200

    Fixes

--- setup.py
+++ setup.py
@@ -2,58 +2,71 @@
 from setuptools import setup
 
 
-CLASSIFIERS = """\
-License :: OSI Approved
-Programming Language :: Python :: 3.10 :: or higher
-Topic :: Software Development
-Operating System :: Microsoft :: Windows
-Operating System :: POSIX
-Operating System :: Unix
-Operating System :: MacOS
-"""
+DISTNAME = "complex_number"
 
-DISTNAME = 'complex_number'
-AUTHOR = 'Enzo Baraldi'
+AUTHOR = "Enzo Baraldi"
 # AUTHOR_EMAIL = ""
-DESCRIPTION = 'This is a simple complex number python package.'
-KEYWORDS = ["python", "python3", "python3.10", "complex number", "complex numbers"]
-LICENSE = 'MIT'
-README = 'This is a simple complex number python package.'
 
-VERSION = '0.1.0'
-ISRELEASED = False
+VERSION = '0.1.1'
+ISRELEASED = True
 
-PYTHON_MIN_VERSION = '3.10'
-# PYTHON_MAX_VERSION = '3.10'
-# PYTHON_REQUIRES = f'>={PYTHON_MIN_VERSION}, <= {PYTHON_MAX_VERSION}'
+DESCRIPTION = "This is a simple complex number python package."
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
+
+URL = "https://github.com/Enzo1603/ComplexNumber"
+
+CLASSIFIERS = [
+    "License:: OSI Approved",
+    "Programming Language:: Python:: 3.10:: or higher",
+    "Topic:: Software Development",
+    "Operating System:: Microsoft:: Windows",
+    "Operating System:: POSIX",
+    "Operating System:: Unix",
+    "Operating System:: MacOS",
+
+]
+
+
+KEYWORDS = ["python", "python 3.10", "complex number", "complex numbers"]
+LICENSE = "MIT"
+
+
+PYTHON_MIN_VERSION = "3.10"
+# PYTHON_MAX_VERSION = "3.10"
+# PYTHON_REQUIRES = f">={PYTHON_MIN_VERSION}, <= {PYTHON_MAX_VERSION}""
 PYTHON_REQUIRES = f">={PYTHON_MIN_VERSION}"
 
 INSTALL_REQUIRES = []
 
 PACKAGES = [
-    'complex_number',
-    'tests',
+    "complex_number",
+    "tests",
 ]
-
-metadata = dict(
-    name=DISTNAME,
-    version=VERSION,
-    long_description=README,
-    packages=PACKAGES,
-    python_requires=PYTHON_REQUIRES,
-    install_requires=INSTALL_REQUIRES,
-    author=AUTHOR,
-    # author_email=AUTHOR_EMAIL,
-    description=DESCRIPTION,
-    keywords=KEYWORDS,
-    classifiers=[CLASSIFIERS],
-    license=LICENSE
-)
 
 
 def setup_package() -> None:
-    setup(**metadata)
+    setup(
+
+        name=DISTNAME,
+        version=VERSION,
+        author=AUTHOR,
+        # author_email=AUTHOR_EMAIL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+        url=URL,
+        classifiers=CLASSIFIERS,
+        packages=PACKAGES,
+        python_requires=PYTHON_REQUIRES,
+        install_requires=INSTALL_REQUIRES,
+        keywords=KEYWORDS,
+        license=LICENSE,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup_package()
